691e57e2f03f5646bc2805420629bb6da17b5768
Dell User <mhmd.dwm@gmail.com>
2026-07-07 22:11:25 +0300

    fix users create password hash

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,21 +1,19 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-
-from app.database import get_db
-from app.models.user import User
-
-router = APIRouter()
-
-# GET USERS
-@router.get("/users")
-def get_users(db: Session = Depends(get_db)):
-    return db.query(User).all()
-
 # CREATE USER
 @router.post("/users")
-def create_user(full_name: str, email: str, db: Session = Depends(get_db)):
-    user = User(full_name=full_name, email=email)
+def create_user(
+    full_name: str,
+    email: str,
+    password_hash: str,
+    db: Session = Depends(get_db)
+):
+    user = User(
+        full_name=full_name,
+        email=email,
+        password_hash=password_hash
+    )
+
     db.add(user)
     db.commit()
     db.refresh(user)
+
     return user
